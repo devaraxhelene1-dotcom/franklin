3eb81407d87fc0ff18c873a8b34422e30e43312b
thibault fages <thibault.sgf@gmail.com>
2026-03-09 17:26:43 +0100

    new

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :projects
+  has_many :projects, dependent: :destroy
+  has_many :chats, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true
 end

8362d9f05e9a2b2b597272c478d5a08c3e170570
thibault fages <thibault.sgf@gmail.com>
2026-03-09 17:01:09 +0100

    models

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,7 @@
 class Campaign < ApplicationRecord
   belongs_to :project
+  has_many :steps, dependent: :destroy
+
+  validates :title, presence: true
+  validates :status, inclusion: { in: %w[draft active completed] }
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many :chats, dependent: :destroy
+  has_many :campaigns, dependent: :destroy
+
+  validates :title, presence: true
 end

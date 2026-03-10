db8a72334d9cbd09a88bd4c7958638d15abf20a8
Francois delPeso <francois_del_peso@hotmail.com>
2026-03-10 16:34:43 +0100

    chat +message controller

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,15 @@
+class ChatsController < ApplicationController
+  def show
+    @chat = Chat.find(params[:id])
+    @message = Message.new
+  end
+
+  def create
+    @chat = Chat.new(title: "Nouvelle campagne", user: current_user)
+    if @chat.save
+      redirect_to chat_path(@chat)
+    else
+      redirect_to root_path, alert: "Impossible de créer le chat."
+    end
+  end
+end

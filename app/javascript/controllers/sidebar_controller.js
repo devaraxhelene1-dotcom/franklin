ab1fefdd75a4be194713d075a6e0b99316aeee2b
helene riverieulx de varax <devarax.helene1@gmail.com>
2026-03-12 14:54:13 +0100

    front sidebar done

---
+++ app/javascript/controllers/sidebar_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["panel"]
+
+  toggle() {
+    this.panelTarget.classList.toggle("collapsed")
+    document.body.classList.toggle("sidebar-collapsed")
+  }
+}

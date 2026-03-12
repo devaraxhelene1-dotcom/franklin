d3bfed50fcbe85fcecb537dae174be48de91b739
helene riverieulx de varax <devarax.helene1@gmail.com>
2026-03-12 15:47:20 +0100

    sidebar menu deroulant done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_sidebar_campaigns
 
   private
 
@@ -12,4 +13,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
+
+  def set_sidebar_campaigns
+    @campaigns = current_user.campaigns if user_signed_in?
+  end
+
 end

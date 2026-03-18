f6491334f65d460fcc0274efaaa3dc438d9b9252
Zoe Sitbon <zoe.sitbon@gmail.com>
2026-03-18 16:53:16 +0100

    lien logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    chat = resource.chats.order(created_at: :desc).first ||
-           resource.chats.create!(title: "Nouvelle campagne")
-    chat_path(chat)
+    dashboard_path
   end
 
   def configure_permitted_parameters
